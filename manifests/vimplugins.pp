be8071691f963a6b7eccb7d94f54b165c738f6bd
Jeff Malnick <malnick@gmail.com>
2016-09-01 13:17:42 -0700

    vim plugins

--- manifests/vimplugins.pp
+++ manifests/vimplugins.pp
@@ -13,7 +13,43 @@ class env::vimplugins (
 		},
 		'solarized' => {
 			'source' => 'https://github.com/altercation/vim-colors-solarized.git'
-		}
+		},
+    'airline' => {
+      'source' => 'https://github.com/vim-airline/vim-airline.git'
+    },
+    'dockerfile' => {
+      'source' => 'https://github.com/ekalinin/Dockerfile.vim'
+    },
+    'json' => {
+      'source' => 'https://github.com/elzr/vim-json'
+    },
+    'puppet' => {
+      'source' => 'https://github.com/rodjek/vim-puppet'
+    },
+    'syntastic' => {
+      'source' => 'https://github.com/rodjek/vim-puppet'
+    }
+    'airline-themes' => {
+      'source' => 'https://github.com/vim-airline/vim-airline-themes'
+    },
+    'flake8' => {
+      'source' => 'https://github.com/nvie/vim-flake8'
+    },
+    'gitgutter' => {
+      'source' => 'https://github.com/airblade/vim-gitgutter'
+    },
+    'go' => {
+      'source' => 'https://github.com/fatih/vim-go'
+    },
+    'pep8-indent' => {
+      'source' => 'https://github.com/hynek/vim-python-pep8-indent',
+    },
+    'vagrant' => {
+      'source' => 'https://github.com/hashivim/vim-vagrant'
+    },
+    'rust' => {
+      'source' => 'https://github.com/rust-lang/rust.vim'
+    }
 	}
 
 	create_resources(vim::plugin, $plugins)
