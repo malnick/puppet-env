1885d16705653f70be2d514348b8c51dc0a16c76
Jeff Malnick <malnick@gmail.com>
2016-09-01 15:58:53 -0700

    Updated gopaths

---
+++ manifests/go.pp
@@ -0,0 +1,27 @@
+class env::go ($home_dir){
+	class {'golang':
+		base_dir    	=> "${home_dir}/projects/go", 
+		from_repo   	=> true,
+		repo_version 	=> 'go1.7',
+		goroot      	=> '$GOPATH/bin:/usr/local/go/bin:$PATH',
+		workdir     	=> "${home_dir}/projects/",
+	} 
+
+  go_paths = {
+    "${home_dir}/projects/go" => {
+      "ensure" => "directory",
+      "owner"  => "malnick",
+      "group"  => "malnick",
+    },
+    "${home_dir}/projects/go/bin" => {
+      "ensure" => "directory",
+      "owner"  => "malnick",
+      "group"  => "malnick",
+    },
+    "${home_dir}/projects/go/src" => {
+      "ensure" => "directory",
+      "owner"  => "malnick",
+      "group"  => "malnick",
+    }
+ 
+}
