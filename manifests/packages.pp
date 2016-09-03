cb20124d1a517e0736a9626a6f1673136babb748
Jeff Malnick <malnick@gmail.com>
2016-09-02 22:14:33 -0700

    spotify pkg needs to requure the ppa

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -9,13 +9,17 @@ class env::packages(
     id      => 'BBEBDCB318AD50EC6865090613B00F1FD2C19886',
     server  => 'hkp://keyserver.ubuntu.com:80',
   }
+  
+  package { 'spotify-client': 
+    ensure  => present,
+    require => Apt::key['spotify'],
+  }
 
   $required_pkgs = {
     'google-chrome-stable'  => {'ensure' => 'present'},
     'build-essential'       => {'ensure' => 'present'},
     'tree'                  => {'ensure' => 'present'},
     'nodejs-legacy'         => {'ensure' => 'present'},
-    'spotify-client'        => {'ensure' => 'present'},
   } 
   
   create_resources(package, $required_pkgs)
