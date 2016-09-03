1a27e4a24726bbdfffe58e4550a4838657168343
Jeff Malnick <malnick@gmail.com>
2016-09-02 21:26:36 -0700

    wine

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,10 +3,14 @@ class env::packages(
 ){
   class { '::tmux': }
 
+  include apt
+  apt::ppa { 'ppa:ubuntu-wine/ppa': }
+
   $required_pkgs = {
     'google-chrome-stable'  => {'ensure' => 'present'},
     'build-essential'       => {'ensure' => 'present'},
     'tree'                  => {'ensure' => 'present'},
+    'wine'                  => {'ensure' => 'present'},
   } 
   
   create_resources(package, $required_pkgs, $pkg_defaults)
