4e8b5ec3573b7900fc27b91317b055c89c8ef18e
Jeff Malnick <malnick@gmail.com>
2016-09-02 22:01:16 -0700

    spotify

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -6,6 +6,10 @@ class env::packages(
 
   include apt
   apt::ppa { 'ppa:ubuntu-wine/ppa': }
+  apt::key { 'spotify':
+    id      => 'BBEBDCB318AD50EC6865090613B00F1FD2C19886',
+    server  => 'hkp://keyserver.ubuntu.com:80',
+  }
 
   $required_pkgs = {
     'google-chrome-stable'  => {'ensure' => 'present'},
@@ -13,6 +17,7 @@ class env::packages(
     'tree'                  => {'ensure' => 'present'},
     'wine'                  => {'ensure' => 'present'},
     'nodejs-legacy'         => {'ensure' => 'present'},
+    'spotify-client'        => {'ensure' => 'present'},
   } 
   
   create_resources(package, $required_pkgs)
