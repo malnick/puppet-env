611f818d9ba310c8c5777b3efd3a41ab08e76584
Jeff Malnick <malnick@gmail.com>
2016-09-06 15:15:12 -0700

    go repors

--- manifests/gitrepos.pp
+++ manifests/gitrepos.pp
@@ -17,8 +17,14 @@ class env::gitrepos($home_dir){
     "${home_dir}/projects/go/src/github.com/dcos/3dt"                               => {'source' => 'git@github.com:dcos/3dt'},
     
     # Personal Golang Repos
-    "${home_dir}/projects/go/src/github.com/malnick/logasaurus"                     => {'source' => 'git@github.com:malnick/logasaurus'},
-    "${home_dir}/projects/go/src/github.com/malnick/cryptorious"                    => {'source' => 'git@github.com:malnick/cryptorious'},
+    "${home_dir}/projects/go/src/github.com/malnick/logasaurus"   => {'source' => 'git@github.com:malnick/logasaurus'},
+    "${home_dir}/projects/go/src/github.com/malnick/cryptorious"  => {'source' => 'git@github.com:malnick/cryptorious'},
+    "${home_dir}/projects/go/src/github.com/malnick/goss"         => {'source' => 'git@github.com:malnick/goss'},
+    "${home_dir}/projects/go/src/github.com/malnick/mux"          => {'source' => 'git@github.com:malnick/mux'},
+    "${home_dir}/projects/go/src/github.com/gorilla/mux"          => {'source' => 'git@github.com:gorilla/mux'},
+    "${home_dir}/projects/go/src/github.com/codegangsta/cli"      => {'source' => 'git@github.com:codegansta/cli'},
+
+
 
     # Personal Projects
     "${home_dir}/projects/technoblogic" => {'source' => 'git@github.com:malnick/technoblogic'},
