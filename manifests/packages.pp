34445d31a544fa62d7ce216bf6ec494c9ec795ac
Jeff Malnick <malnick@gmail.com>
2016-09-07 13:54:14 -0700

    it compiles. good enough!

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -16,11 +16,31 @@ class env::packages(
   }
 
   $required_pkgs = {
-    'google-chrome-stable'  => {'ensure' => 'present'},
-    'build-essential'       => {'ensure' => 'present'},
-    'tree'                  => {'ensure' => 'present'},
-    'nodejs-legacy'         => {'ensure' => 'present'},
+    'google-chrome-stable'          => {'ensure' => 'present'},
+    'build-essential'               => {'ensure' => 'present'},
+    'tree'                          => {'ensure' => 'present'},
+    'nodejs-legacy'                 => {'ensure' => 'present'},
+    'xserver-xorg-input-synaptics'  => {'ensure' => 'absent'},
+    'xserver-xorg-input-libinput'   => {'ensure' => 'present'},
   } 
+  
+  $libinput_config = '
+# Added by scott
+Option "Tapping" "True"
+Option "DisableWhileTyping" "True"
+EndSection
+
+Section "InputClass"
+Identifier "libinput touchscreen catchall"
+MatchIsTouchscreen "on"
+MatchDevicePath "/dev/input/event*"
+Driver "libinput"
+EndSection'
+
+  file_line { '90-libinput.conf': 
+    path => '/usr/share/X11/xorg.conf.d/90-libinput.conf',
+    line => $libinput_config,
+  }
   
   create_resources(package, $required_pkgs)
 
