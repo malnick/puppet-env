19059f05be5d536b04e86c5da1fb73d160cb94d6
Jeff Malnick <malnick@gmail.com>
2016-09-07 14:45:30 -0700

    dotfiles

--- manifests/dotfiles.pp
+++ manifests/dotfiles.pp
@@ -1,7 +1,7 @@
 class env::dotfiles($home_dir){
   vcsrepo { "${home_dir}/personal/dotfiles":
     ensure   => latest,
-    provider => git,
+    provider => 'git',
     source   => 'git@github.com:malnick/dotfiles.git',
     user     => 'malnick',
     require  => User['malnick'],
@@ -15,10 +15,15 @@ class env::dotfiles($home_dir){
   }
 
   $dotfiles = {
-    "${home_dir}/.vimrc"      => {'target' => "${home_dir}/personal/dotfiles/vimrc"}, 
-    "${home_dir}/.tmux.conf"  => {'target' => "${home_dir}/personal/dotfiles/tmux.conf"}, 
-    "${home_dir}/.aws"        => {'target' => "${home_dir}/personal/dotfiles/aws"}, 
-    "${home_dir}/.zshrc"      => {'target'  => "${home_dir}/personal/dotfiles/zshrc"}, 
+    "${home_dir}/.vimrc"        => {'target' => "${home_dir}/personal/dotfiles/vimrc"}, 
+    "${home_dir}/.tmux.conf"    => {'target' => "${home_dir}/personal/dotfiles/tmux.conf"}, 
+    "${home_dir}/.aws"          => {'target' => "${home_dir}/personal/dotfiles/aws"}, 
+    "${home_dir}/.zshrc"        => {'target'  => "${home_dir}/personal/dotfiles/zshrc"}, 
+    "${home_dir}/.oh-my-zsh"    => {'target'  => "${home_dir}/personal/dotfiles/oh-my-zsh"}, 
+    "${home_dir}/.loga"         => {'target'  => "${home_dir}/personal/dotfiles/loga"}, 
+    "${home_dir}/.cryptorious"  => {'target'  => "${home_dir}/personal/dotfiles/cryptorious"}, 
+    "${home_dir}/.gitconfig"    => {'target'  => "${home_dir}/personal/dotfiles/gitconfig"}, 
+
   }
 
   create_resources(file, $dotfiles, $dotfile_defaults)
