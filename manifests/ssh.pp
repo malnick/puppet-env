e7580fcab783f197d555d2ce61e91f308940fc8a
Jeff Malnick <malnick@gmail.com>
2016-09-08 09:24:35 -0700

    kernel and ssh

---
+++ manifests/ssh.pp
@@ -0,0 +1,13 @@
+class env::ssh ($home_dir) {
+  include ssh
+	::ssh::client::config::user { 'malnick':
+		ensure 							=> present,
+		user_home_dir 			=> '/home/malnick',
+		manage_user_ssh_dir => false,
+		require							=> User['malnick'],
+		options 						=> {
+			'StrictHostKeyChecking' => 'no',
+			'UserKnownHostsFile'		=> '/dev/null',
+		}
+	}
+}
