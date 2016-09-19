3d1514d4c84422b19249d15f4a47cac85137bd26
Jeff Malnick <malnick@gmail.com>
2016-09-19 13:28:45 -0700

    heroku clinet

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,6 +3,13 @@ class env::packages(
 ){
   class { '::tmux': }
   class { 'nodejs': }
+  class { 'heroku': 
+    heroku_client_url  => 'http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client.tgz',
+    install_parent_dir => '/usr/local',
+    artifact_dir       => '/usr/local/src/heroku',
+    link_dir           => '/usr/local/heroku',
+    shell              => 'zsh'
+  }
 
   $required_pkgs = {
     'build-essential'               => {'ensure' => 'present'},
