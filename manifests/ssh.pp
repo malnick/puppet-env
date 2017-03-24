0fade5f0f64d23141ff060e36156303a3ebd03be
malnick <malnick@gmail.com>
2017-03-24 09:07:03 -0700

    ssh

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,4 +1,8 @@
 class env::ssh ($home_dir) {
+  class { 'ssh::client':
+    storeconfigs_enabled => false,
+  }
+
 	::ssh::client::config::user { 'malnick':
 		ensure 							=> present,
     manage_user_ssh_dir => false,
