246bc472119a9754d0aae44150904eaf33f18222
malnick <malnick@gmail.com>
2017-03-24 16:20:11 -0700

    use ubuntu user to bootstrap malnick

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,19 +1,14 @@
 class env::ssh ($home_dir) {
-  file { '/home/malnick/.ssh':
-    ensure => directory,
-  }
-
   class { 'ssh::client':
     storeconfigs_enabled => false,
   }
 
-	::ssh::client::config::user { 'malnick':
+	::ssh::client::config::user { 'ubuntu':
 		ensure 							=> present,
     manage_user_ssh_dir => false,
 		options 						=> {
 			'StrictHostKeyChecking' => 'no',
 			'UserKnownHostsFile'		=> '/dev/null',
-		},
-    require             => File['/home/malnick/.ssh'],
+		}
 	}
 }
