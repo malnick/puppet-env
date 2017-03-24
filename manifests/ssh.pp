3fce9df79ec7d6f9aa0047b9441cfc1d0194e0e6
malnick <malnick@gmail.com>
2017-03-24 09:04:46 -0700

    ssh

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,10 +1,7 @@
 class env::ssh ($home_dir) {
-  class { '::ssh':} ->
 	::ssh::client::config::user { 'malnick':
 		ensure 							=> present,
-		user_home_dir 			=> '/home/malnick',
-		manage_user_ssh_dir => false,
-		require							=> User['malnick'],
+    manage_user_ssh_dir => false,
 		options 						=> {
 			'StrictHostKeyChecking' => 'no',
 			'UserKnownHostsFile'		=> '/dev/null',
