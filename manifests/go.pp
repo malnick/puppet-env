e9de5e20d411885bf9d6e34bab11a7f40b0825d7
malnick <malnick@gmail.com>
2018-04-10 15:12:02 -0700

    Use go 1.8

--- manifests/go.pp
+++ manifests/go.pp
@@ -2,7 +2,7 @@ class env::go ($home_dir){
 	class {'golang':
 		base_dir    	=> "${home_dir}/projects/go", 
 		from_repo   	=> true,
-		repo_version 	=> 'go1.7',
+		repo_version 	=> 'go1.8',
 		goroot      	=> '$GOPATH/bin:/usr/local/go/bin:$PATH',
 		workdir     	=> "${home_dir}/projects/",
 	} 
